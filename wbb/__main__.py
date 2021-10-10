d5b503f3da1cc049363df3d03504325a2779b309
SHIVAM-1294 <83240600+SHIVAM-1294@users.noreply.github.com>
2021-10-10 17:12:16 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -133,7 +133,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
 home_text_pm = (
     f"Hey there! My name is {BOT_NAME}. I can manage your "
     + "group with lots of useful features, feel free to "
-    + "add me to your group."
+    + "add me to your group.For music bot help /vchelp"
 )
 
 
